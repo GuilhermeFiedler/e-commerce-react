b06274b74c68788008c7530b647dfe5c2b9c213b
GuilhermeFiedler <lhermetfied@gmail.com>
2026-05-18 14:31:12 -0300

    feat: adiciona função de detalhe de produto.

--- src/components/detalhe/ProdutoDetalhe.jsx
+++ src/components/detalhe/ProdutoDetalhe.jsx
@@ -1,7 +1,38 @@
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 export default function ProdutoDetalhe(){
     const {id} = useParams();
-    const navigate = useNavigate();
-   // const { data: produto, loading } = 
+    const [product, setProduct] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function loadProduct(){
+        try{
+            const res = await api.get(`/products/${id}`)
+
+            setProduct(res.data);
+        } catch(err){
+            console.error(err);
+        } finally {
+            setLoading(false);
+        }
+    }
+        loadProduct();
+}, [id])
+
+    if(loading) {
+        return <p>Carregando...</p>
+    }
+    if(!product){
+        return <p>Produto não encontrado</p>
+    }
+
+    return(
+        <div>
+            <img src={product.image} alt={product.title} />
+            <h1>{product.title}</h1>
+            <p>{product.description}</p>
+            <strong>RS${product.price}</strong>
+        </div>
+    )
 }
