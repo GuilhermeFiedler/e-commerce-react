cbe153f0d29cb7ecac980e007bbc46e0ca892502
GuilhermeFiedler <lhermetfied@gmail.com>
2026-05-20 10:33:24 -0300

    refactor: refatora nomenclatura de pasta

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,7 +1,7 @@
 import { Navigate, createBrowserRouter } from "react-router-dom"
 import Homepage from "../pages/homepage/Homepage"
 import Produtos from "../components/produtos/Produtos"
-import ProdutoDetalhe from "../components/detalhe/ProdutoDetalhe"
+import ProdutoDetalhe from "../components/detalheProduto/ProdutoDetalhe"
 import { LoginPage } from "../pages/loginPage/LoginPage"
 import { RegisterPage } from "../pages/registerPage/RegisterPage"
 import ShopcartPage from "../pages/shopcartPage/ShopcartPage"
